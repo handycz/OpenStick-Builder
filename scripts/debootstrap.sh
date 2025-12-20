991597f6bebe433e7c2b1f8e71f7cfcf147ab7df
Ondrej Novak <ondrej@npost.cz>
2025-12-20 13:41:20 +0100

    Move sysctl config to sysctl.d

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -57,7 +57,7 @@ SUBSYSTEM=="net", ACTION=="add|change|move", ENV{DEVTYPE}=="gadget", ENV{NM_UNMA
 EOF
 
 # install sysctl configuration
-cp configs/sysctl.conf ${CHROOT}/etc/
+cp -r configs/sysctl.d/ ${CHROOT}/etc/
 
 # install kernel
 wget -O - http://mirror.postmarketos.org/postmarketos/v24.06/aarch64/linux-postmarketos-qcom-msm8916-6.6-r5.apk \
